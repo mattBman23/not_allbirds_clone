79f81de7af624928ab3080f84d294c308e64e5af
Matthew Budiman <matthewbudiman@outlook.com>
2021-07-22 16:52:44 -0500

    CHANGE TITLE

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="font-poppins">
       <Head>
-        <title>Allbirds</title>
+        <title>Allbirds TODAY</title>
       </Head>
 
       <BannerMessage />
